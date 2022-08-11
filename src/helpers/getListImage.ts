ba220b5ec739155b02be19245acf99cff901d12b
Usagi88 <ibrahimnaish5677@gmail.com>
2022-08-11 12:03:20 +0500

    feat:(vessel/vehicle) list image

--- src/helpers/getListImage.ts
+++ src/helpers/getListImage.ts
@@ -62,5 +62,31 @@ export function getListImage(name?: string) {
       return require("../images/machine/Welding Genset.png");
     case "Wheel Loader":
       return require("../images/machine/Wheel Loader.jpg");
+    case "Lorry":
+      return require("../images/vehicle/Lorry.jpg");
+    case "Coach Bus":
+      return require("../images/vehicle/Coach Bus.jpg");
+    case "City Bus":
+      return require("../images/vehicle/City Bus.jpg");
+    case "Double Decker":
+      return require("../images/vehicle/Double Decker.jpg");
+    case "Van":
+      return require("../images/vehicle/Van.jpg");
+    case "Pick up":
+      return require("../images/vehicle/Pick up.png");
+    case "Car":
+      return require("../images/vehicle/Car.jpg");
+    case "Speed Boat (25)":
+      return require("../images/vessel/Speed Boat (25).png");
+    case "Speed Boat (47)":
+      return require("../images/vessel/Speed Boat (47).jpg");
+    case "Speed Boat (72)":
+      return require("../images/vessel/Speed Boat (72).jpg");
+    case "Speed Boat (24)":
+      return require("../images/vessel/Speed Boat (24).jpg");
+    case "Ferry (100)":
+      return require("../images/vessel/Ferry (100).jpg");
+    case "Speed Boat (18)":
+      return require("../images/vessel/Speed Boat (18).jpg");
   }
 }
