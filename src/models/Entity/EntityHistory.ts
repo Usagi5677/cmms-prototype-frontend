d9f69b149d6a53a82f0b6071e91e8bf880f570fd
mismaah <mismaahabdulla@gmail.com>
2022-08-18 10:06:21 +0500

    refactor: remove old code

--- src/models/Entity/EntityHistory.ts
+++ src/models/Entity/EntityHistory.ts
@@ -1,18 +1,18 @@
-import { TransportationStatus } from "../Enums";
+import { EntityStatus } from "../Enums";
 import User from "../User";
 import { GraphQLFloat } from "graphql";
 import Location from "../Location";
+import Type from "../Type";
 
 export default interface EntityHistory {
   id: number;
   entityId: number;
-  type: string;
+  type: Type;
   description: string;
   createdAt: Date;
   completedById: number;
   completedBy?: User;
-  transportationStatus: TransportationStatus;
-  transportationType: string;
+  entityStatus: EntityStatus;
   breakdownHour: typeof GraphQLFloat;
   idleHour: typeof GraphQLFloat;
   workingHour: typeof GraphQLFloat;
