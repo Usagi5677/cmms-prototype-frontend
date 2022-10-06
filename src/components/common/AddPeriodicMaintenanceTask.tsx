9d6af6445b46a6dc6c9ac809c26ebeafd0956767
Usagi88 <ibrahimnaish5677@gmail.com>
2022-10-06 18:32:13 +0500

    fix: remove refetch pmSummary when adding task

--- src/components/common/AddPeriodicMaintenanceTask.tsx
+++ src/components/common/AddPeriodicMaintenanceTask.tsx
@@ -27,7 +27,6 @@ export const AddPeriodicMaintenanceTask: React.FC<
       refetchQueries: [
         "getAllPeriodicMaintenanceOfEntity",
         "getAllHistoryOfEntity",
-        "getAllEntityChecklistAndPMSummary",
         "periodicMaintenances"
       ],
     }
