2df62e277d6c3295c221eaf296746c7ac5ed4fd8
Usagi88 <ibrahimnaish5677@gmail.com>
2022-10-19 23:12:46 +0500

    fix: typo

--- src/components/Templates/CreatePeriodicMaintenance.tsx
+++ src/components/Templates/CreatePeriodicMaintenance.tsx
@@ -142,7 +142,7 @@ export const CreatePeriodicMaintenance: React.FC<
                 <>
                   Value
                   <span style={{ paddingLeft: 10, opacity: 0.5 }}>
-                    For example, value = 3 will be every hour 3 / km 3
+                    For example, value = 3 will be every 3 hour / 3 km
                   </span>
                 </>
               }
