980af0e4caaa2e3b3f85f1a2f1e072cdf9a71963
Usagi88 <ibrahimnaish5677@gmail.com>
2022-11-10 12:49:24 +0500

    fix: entity card in view entity

--- src/pages/Entity/ViewEntity/ViewSubEntity/ViewSubEntity.tsx
+++ src/pages/Entity/ViewEntity/ViewSubEntity/ViewSubEntity.tsx
@@ -6,6 +6,7 @@ import { DELETE_ENTITY } from "../../../../api/mutations";
 import { DeleteListing } from "../../../../components/common/DeleteListing";
 import EntityStatusTag from "../../../../components/common/EntityStatusTag";
 import EditEntity from "../../../../components/EntityComponents/EditEntity/EditEntity";
+import EntityCard from "../../../../components/EntityComponents/EntityCard/EntityCard";
 import UserContext from "../../../../contexts/UserContext";
 import { isAssignedType } from "../../../../helpers/permissions";
 import { useIsSmallDevice } from "../../../../helpers/useIsSmallDevice";
@@ -34,97 +35,7 @@ const ViewSubEntity = ({
         >
           <div className={classes["info-container"]}>
             {subEntity?.subEntities?.map((s: Entity, index: number) => (
-              <div className={classes["info-wrapper"]} key={s.id}>
-                <div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}></div>
-                    <div className={classes["options"]}>
-                      {isAssignedType("Admin", subEntity, self) ? (
-                        <EditEntity
-                          entity={s}
-                          isDeleted={isDeleted}
-                          fontSize={14}
-                          includeSubEntity
-                        />
-                      ) : null}
-                      {isAssignedType("Admin", subEntity, self) ? (
-                        <div className={classes["button"]}>
-                          <DeleteListing
-                            id={s.id}
-                            mutation={DELETE_ENTITY}
-                            refetchQueries={["getAllEntity", "getSingleEntity"]}
-                          />
-                        </div>
-                      ) : null}
-                      <Link to={"/entity/" + s.id}>
-                        <Tooltip title="Open">
-                          <FaArrowAltCircleRight
-                            className={classes["button"]}
-                          />
-                        </Tooltip>
-                      </Link>
-                    </div>
-                  </div>
-                  <div className={classes["info"]} style={{ marginTop: 10 }}>
-                    <div className={classes["info-title"]}>Status</div>
-                    <div className={classes["info-content"]}>
-                      <EntityStatusTag status={s.status} noMarginRight />
-                    </div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>Machine Number</div>
-                    <div className={classes["info-content"]}>
-                      {s.machineNumber}
-                    </div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>Model</div>
-                    <div className={classes["info-content"]}>{s.model}</div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>Type</div>
-                    <div className={classes["info-content"]}>
-                      {s.type?.name}
-                    </div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>Brand</div>
-                    <div className={classes["info-content"]}>{s.brand}</div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>
-                      Current running {s.measurement}
-                    </div>
-                    <div className={classes["info-content"]}>
-                      {s.currentRunning}
-                    </div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>
-                      Last service {s.measurement}
-                    </div>
-                    <div className={classes["info-content"]}>
-                      {s.lastService}
-                    </div>
-                  </div>
-                  <div className={classes["info"]}>
-                    <div className={classes["info-title"]}>
-                      Inter service {s.measurement}
-                    </div>
-                    <div className={classes["info-content"]}>
-                      {(s?.currentRunning ?? 0) - (s?.lastService ?? 0)}
-                    </div>
-                  </div>
-                </div>
-                {index + 1 !== subEntity?.subEntities?.length && (
-                  <Divider
-                    style={{
-                      height: "100%",
-                    }}
-                    type={isSmallDevice ? "vertical" : "horizontal"}
-                  />
-                )}
-              </div>
+              <EntityCard entity={s} key={s.id} />
             ))}
           </div>
         </Collapse.Panel>
