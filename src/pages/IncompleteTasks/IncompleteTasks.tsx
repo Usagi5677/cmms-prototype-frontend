503bcb77101b8cc3ec910efa318e5abd0d48435a
Usagi88 <ibrahimnaish5677@gmail.com>
2022-11-15 23:35:18 +0500

    fix: remove graph test

--- src/pages/IncompleteTasks/IncompleteTasks.tsx
+++ src/pages/IncompleteTasks/IncompleteTasks.tsx
@@ -46,7 +46,6 @@ export const IncompleteTasks: React.FC<IncompleteTasksProps> = ({}) => {
         ></Tabs.TabPane> */}
         </Tabs>
       </div>
-      <GroupedLocationIncompleteTask />
     </>
   );
 };
