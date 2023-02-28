4cd72eb024ba0134829a2bf41be36666ee591a07
Usagi88 <ibrahimnaish5677@gmail.com>
2023-02-28 21:12:45 +0500

    style(view entity): adjustments

--- src/components/EntityComponents/EntityUsageHistory/EntityUsageHistory.tsx
+++ src/components/EntityComponents/EntityUsageHistory/EntityUsageHistory.tsx
@@ -145,8 +145,8 @@ const EntityUsageHistory = ({ entity }: { entity?: Entity }) => {
         <DatePicker.RangePicker
           defaultValue={dates}
           format={DATETIME_FORMATS.DAY_MONTH_YEAR}
-          style={{ width: 350, borderRadius: 20 }}
-          popupStyle={{ borderRadius: 20 }}
+          style={{ width: 350, borderRadius: 6 }}
+          popupStyle={{ borderRadius: 6 }}
           disabledDate={(date) => date.isAfter(moment(), "day")}
           onChange={setDates}
           allowClear={false}
